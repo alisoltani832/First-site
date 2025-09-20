7b770320388b6e856cccf50aff5bb52285a047f8
alisoltani832 <alisoltani832@gmail.com>
2025-09-21 01:35:39 +0330

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>سلام! این سایت نمونه علی سلطانی است 🚀</h1>
+      <p>ساخته شده با React + Vite + Vercel</p>
+    </div>
+  )
+}
